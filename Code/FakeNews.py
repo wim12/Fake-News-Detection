098e8418d5fdc05254af701f8fb77438a8c4b95a
wim12 <wim12@aber.ac.uk>
2020-03-15 12:37:52 +0000

    Added more modifications to code. 6/7's of models and ROC curve added

--- Code/FakeNews.py
+++ Code/FakeNews.py
@@ -2,11 +2,19 @@ import itertools
 
 import pandas as pd
 from sklearn import metrics
+from sklearn import tree
+from sklearn.datasets import load_iris
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import RidgeClassifier, SGDClassifier
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import label_binarize
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 
 dataset = 'fake_or_real_news.csv'
 
@@ -149,27 +157,166 @@ clf = MultinomialNB()
 
 clf.fit(tfidf_train, y_train)
 pred = clf.predict(tfidf_test)
-score = metrics.accuracy_score(y_test, pred)
-print("accuracy:   %0.3f" % score)
-cm = metrics.confusion_matrix(y_test, pred, labels=['FAKE', 'REAL'])
-cmfass = plot_confusion_matrix(cm, classes=['FAKE', 'REAL'])
-tfi_f1 = metrics.f1_score(y_test, pred, average='macro')
-print('f1 score: ', tfi_f1)
-tfi_acc = metrics.accuracy_score(y_test, pred)
-print('accuracy: ', tfi_acc)
-tfi_prec = metrics.precision_score(y_test, pred, average='micro')
-print('precision: ', tfi_prec)
 
-print(metrics.classification_report(y_test,pred, labels=['FAKE', 'REAL']))
 
-clf = MultinomialNB()
+def printMets(y_test, pred):
+    score = metrics.accuracy_score(y_test, pred)
+    print("accuracy:   %0.3f" % score)
+    cm = metrics.confusion_matrix(y_test, pred, labels=['FAKE', 'REAL'])
+    plot_confusion_matrix(cm, classes=['FAKE', 'REAL'])
+    tfi_f1 = metrics.f1_score(y_test, pred, average='macro')
+    print('f1 score: ', tfi_f1)
+    tfi_acc = metrics.accuracy_score(y_test, pred)
+    print('accuracy: ', tfi_acc)
+    tfi_prec = metrics.precision_score(y_test, pred, average='micro')
+    print('precision: ', tfi_prec)
 
-clf.fit(count_train, y_train)
-pred = clf.predict(count_test)
-score = metrics.accuracy_score(y_test, pred)
-print("accuracy:   %0.3f" % score)
-cm = metrics.confusion_matrix(y_test, pred, labels=['FAKE', 'REAL'])
-plot_confusion_matrix(cm, classes=['FAKE', 'REAL'])
+    print(metrics.classification_report(y_test, pred, labels=['FAKE', 'REAL']))
+
+mnbtestct = MultinomialNB()
+mnbtestct.fit(count_train, y_train)
+mnbpredct = mnbtestct.predict(count_test)
+printMets(y_test, mnbpredct)
+
+conmat = metrics.confusion_matrix(y_test, mnbpredct, labels=['FAKE', 'REAL'])
+plot_confusion_matrix(conmat, classes=['FAKE', 'REAL'])
+metrics.plot_roc_curve(mnbtestct, count_test, y_test)
+plt.show()
+metrics.plot_precision_recall_curve(mnbtestct, count_test, y_test)
+plt.show()
+
+print("SGD Classifier: Tfidf")
+
+sgdtesttfi = SGDClassifier(loss="hinge", shuffle=True)
+sgdtesttfi.fit(tfidf_train, y_train)
+sgdpredtfi = sgdtesttfi.predict(tfidf_test)
+print(classification_report(y_test, sgdpredtfi, labels=['FAKE', 'REAL']))
+
+print("SGD Classifier: Count Vectorizer")
+
+sgdtestct = SGDClassifier(loss="hinge", shuffle=True)
+sgdtestct.fit(count_train, y_train)
+sgdpredct = sgdtestct.predict(count_test)
+print(classification_report(y_test, sgdpredct, labels=['FAKE', 'REAL']))
+
+print("K Nearest Neighbours Classifier: Tfidf: K = 3")
+
+kneightesttf = KNeighborsClassifier(n_neighbors=3)
+kneightesttf.fit(tfidf_train, y_train)
+knpredtf = kneightesttf.predict(tfidf_test)
+print(classification_report(y_test, knpredtf, labels=['FAKE', 'REAL']))
+
+print("K Nearest Classifier: Count Vectorizer: K = 3")
+
+kneightestct = KNeighborsClassifier(n_neighbors=3)
+kneightestct.fit(count_train, y_train)
+knpredct = kneightestct.predict(count_test)
+print(classification_report(y_test, knpredct, labels=['FAKE', 'REAL']))
+
+print("K Nearest Neighbours Classifier: Tfidf: K = 5")
+
+kneightesttf = KNeighborsClassifier(n_neighbors=5)
+kneightesttf.fit(tfidf_train, y_train)
+knpredtf = kneightesttf.predict(tfidf_test)
+print(classification_report(y_test, knpredtf, labels=['FAKE', 'REAL']))
+
+print("K Nearest Classifier: Count Vectorizer: K = 5")
+
+kneightestct = KNeighborsClassifier(n_neighbors=5)
+kneightestct.fit(count_train, y_train)
+knpredct = kneightestct.predict(count_test)
+print(classification_report(y_test, knpredct, labels=['FAKE', 'REAL']))
+
+print("Ridge Classifier: Tfidf")
+
+ridgeclasstest = RidgeClassifier()
+ridgeclasstest.fit(tfidf_train, y_train)
+rcpredtf = ridgeclasstest.predict(tfidf_test)
+print(classification_report(y_test, rcpredtf, labels=['FAKE', 'REAL']))
+
+print("Ridge Classifier: Count Vectorizor")
+
+ridgeclasstest = RidgeClassifier()
+ridgeclasstest.fit(count_train, y_train)
+rcpredct = ridgeclasstest.predict(count_test)
+print(classification_report(y_test, rcpredct, labels=['FAKE', 'REAL']))
+
+print("Decision Tree Classifier: Tfidf: Max Depth 4")
+
+dectreetest = DecisionTreeClassifier(max_depth=4)
+dectreetest = dectreetest.fit(tfidf_train, y_train)
+decpredtf = dectreetest.predict(tfidf_test)
+plt.figure(figsize=(20, 20))
+plot_tree(dectreetest)
+print(classification_report(y_test, decpredtf, labels=['FAKE', 'REAL']))
+
+plt.show()
+
+print("Decision Tree Classifier: Count Vectorizer: Max Depth 4")
+
+dectreetest = DecisionTreeClassifier(max_depth=4)
+dectreetest = dectreetest.fit(count_train, y_train)
+decpredct = dectreetest.predict(count_test)
+plt.figure(figsize=(20, 20))
+plot_tree(dectreetest)
+print(classification_report(y_test, decpredct, labels=['FAKE', 'REAL']))
+
+plt.show()
+
+print("Decision Tree Classifier: Tfidf: Max Depth 8")
+
+dectreetest = DecisionTreeClassifier(max_depth=8)
+dectreetest = dectreetest.fit(tfidf_train, y_train)
+decpredtf = dectreetest.predict(tfidf_test)
+plt.figure(figsize=(20, 20))
+plot_tree(dectreetest)
+print(classification_report(y_test, decpredtf, labels=['FAKE', 'REAL']))
+
+plt.show()
+
+print("Decision Tree Classifier: Count Vectorizer: Max Depth 8")
+
+dectreetest = DecisionTreeClassifier(max_depth=8)
+dectreetest = dectreetest.fit(count_train, y_train)
+decpredct = dectreetest.predict(count_test)
+plt.figure(figsize=(20, 20))
+plot_tree(dectreetest)
+print(classification_report(y_test, decpredct, labels=['FAKE', 'REAL']))
+
+plt.show()
+
+print("Linear SVC Classifier: Tfidf: Random State 0")
+
+svctesttf = LinearSVC(random_state=0)
+svctesttf.fit(tfidf_train, y_train)
+svcpredtf = svctesttf.predict(tfidf_test)
+printMets(y_test, svcpredtf)
+
+print("Linear SVC Classifier: Count Vectorizer: Random State 0")
+
+svctestct = LinearSVC(random_state=0)
+svctestct.fit(tfidf_train, y_train)
+svcpredct = svctestct.predict(tfidf_test)
+printMets(y_test, pred)
+
+print("Multinomial Naive-Bayes Classifier: Tfidf")
+
+mnbtesttf = MultinomialNB()
+mnbtesttf.fit(tfidf_train, y_train)
+mnbpredtf = mnbtesttf.predict(tfidf_test)
+printMets(y_test, mnbpredtf)
+
+print("Multinomial Naive-Bayes Classifier: Count")
+
+mnbtestct = MultinomialNB()
+mnbtestct.fit(count_train, y_train)
+mnbpredct = mnbtestct.predict(count_test)
+printMets(y_test, mnbpredct)
+
+conmat = metrics.confusion_matrix(y_test, mnbpredct, labels=['FAKE', 'REAL'])
+plot_confusion_matrix(conmat, classes=['FAKE', 'REAL'])
+aa = metrics.plot_roc_curve(mnbpredct, count_test, y_test)
+plt.show()
 
 sxx = fakeRealPanda["title"]
 print(sxx)
